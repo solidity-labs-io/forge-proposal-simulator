58c27fd8b1f4c25da1f6e8b5e7b3857422b0c7e7
Ana Julia <anajuliabit@gmail.com>
2023-12-22 13:44:41 -0300

    add warning to TimelockController

--- utils/TimelockController.sol
+++ utils/TimelockController.sol
@@ -4,19 +4,8 @@ pragma solidity 0.8.19;
 
 import {Address} from "@utils/Address.sol";
 
-/**
- * @dev Contract module which acts as a timelocked controller. When set as the
- * owner of an `Ownable` smart contract, it enforces a timelock on all
- * `onlyOwner` maintenance operations. This gives time for users of the
- * controlled contract to exit before a potentially dangerous maintenance
- * operation is applied.
- *
- * By default, this contract is self administered, meaning administration tasks
- * have to go through the timelock process. The proposer (resp executor) role
- * is in charge of proposing (resp executing) operations. A common use case is
- * to position this {TimelockController} as the owner of a smart contract, with
- * a multisig or a DAO as the sole proposer.
- */
+// @dev This file is for testing purposes only and lacks full functionality and access control features.
+// For production, use the complete version from OpenZeppelin.
 contract TimelockController {
     uint256 internal constant _DONE_TIMESTAMP = uint256(1);
 
