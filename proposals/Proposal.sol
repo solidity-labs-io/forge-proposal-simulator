0f7d86e7583e217ec0d7fd0eea4e54bebadf45ee
Elliot <elliotfriedman3@gmail.com>
2024-03-18 15:26:20 -0700

    add alpha proposal
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- proposals/Proposal.sol
+++ proposals/Proposal.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.8.0;
 
+import {Strings} from "@openzeppelin/utils/Strings.sol";
+
 import {Test} from "@forge-std/Test.sol";
-import {IProposal} from "@proposals/IProposal.sol";
-import {Script} from "@forge-std/Script.sol";
-import {Addresses} from "@addresses/Addresses.sol";
+import {VmSafe} from "@forge-std/Vm.sol";
 import {console} from "@forge-std/console.sol";
 
+import {Script} from "@forge-std/Script.sol";
+import {IProposal} from "@proposals/IProposal.sol";
+import {Addresses} from "@addresses/Addresses.sol";
+
 abstract contract Proposal is Test, Script, IProposal {
+    using Strings for *;
+
     struct Action {
         address target;
         uint256 value;
@@ -14,12 +20,100 @@ abstract contract Proposal is Test, Script, IProposal {
         string description;
     }
 
+    /// @notice starting snapshot of the contract state before the calls are made
+    uint256 private _startSnapshot;
+
     /// @notice list of actions to be executed, regardless of proposal type
     /// they all follow the same structure
     Action[] public actions;
 
     /// @notice debug flag to print proposal actions and steps
     bool internal DEBUG;
+
+    /// @notice to be used by the build function easily create a governance proposal
+    modifier buildModifier(address caller, Addresses addresses) {
+        _startBuild(caller);
+        _;
+        _endBuild(caller, addresses);
+    }
+
+    /// @notice to be used by the build function to create a governance proposal
+    /// kick off the process of creating a governance proposal by:
+    ///  1). taking a snapshot of the current state of the contract
+    ///  2). starting prank as the caller
+    ///  3). starting a recording of all calls created during the proposal
+    function _startBuild(address caller) private {
+        _startSnapshot = vm.snapshot();
+        vm.startPrank(caller);
+        vm.startStateDiffRecording();
+    }
+
+    /// @notice to be used at the end of the build function to snapshot
+    /// the actions performed by the proposal and revert these changes
+    /// then, stop the prank and record the actions that were taken by the proposal.
+    function _endBuild(address caller, Addresses addresses) private {
+        vm.stopPrank();
+        VmSafe.AccountAccess[] memory accountAccesses = vm
+            .stopAndReturnStateDiff();
+
+        /// roll back all state changes made during the governance proposal
+        require(
+            vm.revertTo(_startSnapshot),
+            "failed to revert back to snapshot, unsafe state to run proposal"
+        );
+
+        for (uint256 i = 0; i < accountAccesses.length; i++) {
+            /// only care about calls from the original caller,
+            /// static calls are ignored,
+            /// calls to and from Addresses and the vm contract are ignored
+            if (
+                accountAccesses[i].account != address(addresses) &&
+                accountAccesses[i].account != address(vm) && /// ignore calls to vm in the build function
+                accountAccesses[i].accessor != address(addresses) &&
+                accountAccesses[i].kind == VmSafe.AccountAccessKind.Call &&
+                accountAccesses[i].accessor == caller /// caller is correct, not a subcall
+            ) {
+                _pushAction(
+                    accountAccesses[i].value,
+                    accountAccesses[i].account,
+                    accountAccesses[i].data,
+                    string(
+                        abi.encodePacked(
+                            "calling ",
+                            accountAccesses[i].account.toHexString(),
+                            " with ",
+                            accountAccesses[i].value.toString(),
+                            " eth and ",
+                            _bytesToString(accountAccesses[i].data),
+                            " data."
+                        )
+                    )
+                );
+            }
+        }
+    }
+
+    /// @notice convert bytes to a string
+    /// @param data the bytes to convert to a human readable string
+    function _bytesToString(
+        bytes memory data
+    ) private pure returns (string memory) {
+        /// Initialize an array of characters twice the length of data,
+        /// since each byte will be represented by two hexadecimal characters
+        bytes memory buffer = new bytes(data.length * 2);
+
+        /// Characters for conversion
+        bytes memory characters = "0123456789abcdef";
+
+        for (uint256 i = 0; i < data.length; i++) {
+            /// For each byte, find the corresponding hexadecimal characters
+            buffer[i * 2] = characters[uint256(uint8(data[i] >> 4))];
+            buffer[i * 2 + 1] = characters[uint256(uint8(data[i] & 0x0f))];
+        }
+
+        /// Convert the bytes array to a string and return
+        return string(buffer);
+    }
 
     /// @notice override this to set the proposal name
     function name() external view virtual returns (string memory);
@@ -136,6 +230,12 @@ abstract contract Proposal is Test, Script, IProposal {
         }
     }
 
+    /// --------------------------------------------------------------------
+    /// --------------------------------------------------------------------
+    /// ------------------------ Legacy FPS Support ------------------------
+    /// --------------------------------------------------------------------
+    /// --------------------------------------------------------------------
+
     /// @dev push an action to the proposal
     function _pushAction(
         uint256 value,
@@ -175,6 +275,12 @@ abstract contract Proposal is Test, Script, IProposal {
     function _pushAction(address target, bytes memory data) internal {
         _pushAction(0, target, data, "");
     }
+
+    /// --------------------------------------------------------------------
+    /// --------------------------------------------------------------------
+    /// ------------------ Internal functions to override ------------------
+    /// --------------------------------------------------------------------
+    /// --------------------------------------------------------------------
 
     /// @dev Deploy contracts and add them to list of addresses
     function _deploy(Addresses, address) internal virtual {}
