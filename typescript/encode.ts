8668a79e85151495bc7cde387887c026aa81077f
prateek <prateekgupta105@gmail.com>
2024-03-19 20:23:44 +0530

    pr feedback

--- typescript/encode.ts
+++ typescript/encode.ts
@@ -1,15 +1,6 @@
 import { ethers } from 'ethers';
 import * as fs from 'fs';
 
-const args = process.argv;
-
-// Removes '\' from command line args
-const cleanedArg = args[2].replace(/\\/g, "");
-
-const constructorInputs = JSON.parse(cleanedArg);
-
-const artifactPath = args[3];
-
 function extractABI(artifactPath: string): any {
 	artifactPath = artifactPath.replace(":", "/");
 
@@ -27,54 +18,64 @@ function extractABI(artifactPath: string): any {
     }
 }
 
-const abi = extractABI(artifactPath);
+// Recursive function to traverse and generate type array
+function traverseInputs(inputs: any, depth: number): any {
+	if (depth == 0) {
+		return inputs.map((input: any) => {
+			if (input.type === 'tuple[]') {
+				return `tuple(${traverseInputs(input.components, depth + 1)})[]`
+			}
+			if (input.type === 'tuple') {
+				return `tuple(${traverseInputs(input.components, depth + 1)})`;
+			} else {
+				return input.type;
+			}
+		});
+	}
+	return inputs.map((input: any) => {
+		if (input.type === 'tuple[]') {
+			return `tuple(${traverseInputs(input.components, depth + 1)})[]`
+		}
+		else if (input.type === 'tuple') {
+			return `tuple(${traverseInputs(input.components, depth + 1)})`;
+		} else {
+			return input.type;
+		}
+	}).join(', ');
+}
+
+// Function to generate type array based on the ABI format
+function generateTypeArray(abi: any[]): string[] {
+	const constructorAbi = abi.find(item => item.type === 'constructor');
+	if (!constructorAbi) {
+		throw new Error(`Constructor not found in ABI.`);
+	}
+
+	const inputs = constructorAbi.inputs;
+	
+	const typeArray: string[] = traverseInputs(inputs, 0);
+	return typeArray;
+}
 
 // Function to encode data based on the ABI format
 function encodeData(abi: any[], constructorInputs: any[]): string {
-
-    // Function to generate type array based on the ABI format
-    function generateTypeArray(abi: any[]): string[] {
-        const constructorAbi = abi.find(item => item.type === 'constructor');
-        if (!constructorAbi) {
-            throw new Error(`Constructor not found in ABI.`);
-        }
-
-        const inputs = constructorAbi.inputs;
-
-        // Recursive function to traverse and generate type array
-        function traverseInputs(inputs: any, depth: number): any {
-            if (depth == 0) {
-                return inputs.map((input: any) => {
-					if (input.type === 'tuple[]') {
-						return `tuple(${traverseInputs(input.components, depth + 1)})[]`
-					}
-                    if (input.type === 'tuple') {
-                        return `tuple(${traverseInputs(input.components, depth + 1)})`;
-                    } else {
-                        return input.type;
-                    }
-                });
-            }
-            return inputs.map((input: any) => {
-				if (input.type === 'tuple[]') {
-					return `tuple(${traverseInputs(input.components, depth + 1)})[]`
-				}
-                else if (input.type === 'tuple') {
-                    return `tuple(${traverseInputs(input.components, depth + 1)})`;
-                } else {
-                    return input.type;
-                }
-            }).join(', ');
-        }
-        
-        const typeArray: string[] = traverseInputs(inputs, 0);
-        return typeArray;
-    }
 
     const types = generateTypeArray(abi);
 
     return ethers.utils.defaultAbiCoder.encode(types, constructorInputs);
 }
+
+// Gets command line arguments
+const args = process.argv;
+
+// Removes '\' from command line args
+const cleanedArg = args[2].replace(/\\/g, "");
+
+const constructorInputs = JSON.parse(cleanedArg);
+
+const artifactPath = args[3];
+
+const abi = extractABI(artifactPath);
 
 // Encode the constructor inputs
 const encodedData = encodeData(abi, constructorInputs);
