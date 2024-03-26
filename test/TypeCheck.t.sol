491ab6f5154661963bba91e23087e8579a7273d3
prateek <prateekgupta105@gmail.com>
2024-03-26 18:34:25 +0530

    Add failure test for incorrect deployed bytecode

--- test/TypeCheck.t.sol
+++ test/TypeCheck.t.sol
@@ -10,6 +10,8 @@ contract TypeCheck is Test {
     string public constant ARTIFACT_DIRECTORY = "out/";
     string public constant TYPE_CHECK_ADDRESSES_PATH_INCORRECT =
         "addresses/TypeCheckAddressesIncorrect.json";
+    string public constant ADDRESSES_PATH_INCORRECT =
+        "addresses/AddressesIncorrect.json";
     TypeChecker public typechecker;
 
     function test_typeCheck() public {
@@ -21,13 +23,23 @@ contract TypeCheck is Test {
     }
 
     // Artifact path is incorrect in TypeCheckAddresses.json
-    function test_typeCheckIncorrect() public {
+    function test_typeCheckIncorrectArtifact() public {
         vm.expectRevert(
             "StdCheats deployCode(string,bytes): Deployment failed."
         );
         typechecker = new TypeChecker(
             ADDRESSES_PATH,
             TYPE_CHECK_ADDRESSES_PATH_INCORRECT,
+            ARTIFACT_DIRECTORY
+        );
+    }
+
+    // Deployed bytecode is incorrect
+    function test_typeCheckIncorrectByteCode() public {
+        vm.expectRevert("Deployed bytecode not matched");
+        typechecker = new TypeChecker(
+            ADDRESSES_PATH_INCORRECT,
+            TYPE_CHECK_ADDRESSES_PATH,
             ARTIFACT_DIRECTORY
         );
     }
