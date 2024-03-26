f92b45715ad7283c53027e5fa506f0ad644e5559
prateek <prateekgupta105@gmail.com>
2024-03-26 18:14:06 +0530

    Add failure test for incorrect artifact path

--- test/TypeCheck.t.sol
+++ test/TypeCheck.t.sol
@@ -4,19 +4,31 @@ import {TypeChecker} from "@type-check/TypeChecker.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract TypeCheck is Test {
-    string public constant ADDRESSES_PATH = "../../addresses/Addresses.json";
+    string public constant ADDRESSES_PATH = "addresses/Addresses.json";
     string public constant TYPE_CHECK_ADDRESSES_PATH =
-        "../../addresses/TypeCheckAddresses.json";
+        "addresses/TypeCheckAddresses.json";
+    string public constant ARTIFACT_DIRECTORY = "out/";
+    string public constant TYPE_CHECK_ADDRESSES_PATH_INCORRECT =
+        "addresses/TypeCheckAddressesIncorrect.json";
     TypeChecker public typechecker;
 
-    function setUp() public {
+    function test_typeCheck() public {
         typechecker = new TypeChecker(
             ADDRESSES_PATH,
-            TYPE_CHECK_ADDRESSES_PATH
+            TYPE_CHECK_ADDRESSES_PATH,
+            ARTIFACT_DIRECTORY
         );
     }
 
-    function test_true() public {
-        assertTrue(true);
+    // Artifact path is incorrect in TypeCheckAddresses.json
+    function test_typeCheckIncorrect() public {
+        vm.expectRevert(
+            "StdCheats deployCode(string,bytes): Deployment failed."
+        );
+        typechecker = new TypeChecker(
+            ADDRESSES_PATH,
+            TYPE_CHECK_ADDRESSES_PATH_INCORRECT,
+            ARTIFACT_DIRECTORY
+        );
     }
 }
