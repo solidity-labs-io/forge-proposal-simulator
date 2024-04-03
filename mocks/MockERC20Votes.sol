143907fbf1ba2be299e7b055124ddd3831695b7e
Ana Julia <anajuliabit@gmail.com>
2024-04-03 14:08:54 -0300

    bravo deploy

---
+++ mocks/MockERC20Votes.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20, ERC20Permit, ERC20VotesComp} from "@openzeppelin/token/ERC20/extensions/ERC20VotesComp.sol";
+
+contract MockERC20Votes is ERC20VotesComp {
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20Permit(name_) ERC20(name_, symbol_) {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
