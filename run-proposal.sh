6ae9c3ffe87e5c1c29ffc8ea3dfe49cee95937ef
Ana Julia <anajuliabit@gmail.com>
2024-04-12 11:48:40 -0300

    update

--- run-proposal.sh
+++ run-proposal.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Assuming PR_CHANGED_FILES contains paths of files changed in the PR
 CHANGED_FILES=$PR_CHANGED_FILES
 
-# If there are changed files, loop through them and execute your command
 if [[ ! -z "$CHANGED_FILES" ]]; then
     # Splitting the file paths into an array
     IFS=' ' read -r -a files_array <<< "$CHANGED_FILES"
@@ -16,9 +14,10 @@ if [[ ! -z "$CHANGED_FILES" ]]; then
             output=$(forge script "$file" 2>&1)
             # Strip ANSI escape codes
             clean_output=$(echo "$output" | sed 's/\x1b\[[0-9;]*m//g')
-            # Convert to JSON using jq
+            # Convert to JSON using jq, ensuring correct JSON output
             json_output=$(jq -n --arg file "$file" --arg output "$clean_output" '{file: $file, output: $output}')
-            echo "$json_output" > output.json
+            # Directly print the JSON output for GitHub Actions to capture
+            echo "$json_output"
             break  # Exit the loop after processing the first file
         fi
     done
