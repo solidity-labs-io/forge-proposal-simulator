6fa276884e28ffefd2e423c796914ca3bd3bdacb
prateek <prateekgupta105@gmail.com>
2024-08-16 21:08:04 +0530

    add unit test for multisig proposal

---
+++ mocks/MockAuction.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+contract MockAuction {
+    address public highestBidder;
+    uint public highestBid;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function bid() public payable {
+        require(msg.value > highestBid, "Bid not high enough");
+        if (highestBidder != address(0)) {
+            payable(highestBidder).transfer(highestBid);
+        }
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+    }
+
+    function endAuction() public {
+        require(msg.sender == owner, "Only owner can end auction");
+        payable(owner).transfer(highestBid);
+    }
+}
---
+++ mocks/MockSavingContract.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.0;
+
+contract MockSavingContract {
+    struct Deposit {
+        uint256 amount;
+        uint256 unlockTime;
+    }
+
+    mapping(address => Deposit[]) public deposits;
+
+    function deposit(uint256 lockTime) public payable {
+        require(msg.value > 0, "Must send some Ether");
+        deposits[msg.sender].push(
+            Deposit({amount: msg.value, unlockTime: block.timestamp + lockTime})
+        );
+    }
+
+    function withdraw(uint256 depositIndex) public {
+        Deposit memory userDeposit = deposits[msg.sender][depositIndex];
+        require(
+            block.timestamp >= userDeposit.unlockTime,
+            "Deposit is still locked"
+        );
+        require(userDeposit.amount > 0, "No funds to withdraw");
+
+        uint256 amount = userDeposit.amount;
+        deposits[msg.sender][depositIndex].amount = 0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getDeposit(
+        address user,
+        uint256 depositIndex
+    ) public view returns (uint256 amount, uint256 unlockTime) {
+        return (
+            deposits[user][depositIndex].amount,
+            deposits[user][depositIndex].unlockTime
+        );
+    }
+}
